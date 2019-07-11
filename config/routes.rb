20041daf2049f818c9123200198f483cb734681b
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2019-07-11 04:07:20 +0000

    ユーザーのルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  namespace :admin do
+    resources :users do
+      post :confirm, action: :confirm_new, on: :new
+    end
+  end
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
