c83754016df638cc9c1a2de16037b390cae47c3f
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2019-07-19 06:27:23 +0000

    ransackの関連の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,9 @@ class User < ApplicationRecord
         %w[name email]
     end
     
-    def self.ransackable_associations(auth_object = nil)
-        []
-    end
-    
-    
-    validates :name, presence: true, length: {maximum: 15}
+    validates :name, presence: true, length: { maximum: 15 }
     validates :email, presence: true, uniqueness: true
-    validates :profile, length: {maximum: 1000}
-    validates :password, length: {in: 4..15}, presence: true
+    validates :profile, length: { maximum: 1000 }
+    validates :password, length: { in: 4..15 }, presence: true
     
 end
