9349f701a18e438baed0d194313b42b9a376ceea
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2019-07-23 05:57:55 +0000

    weコントローラーの追加

--- app/controllers/work_experiences_controller.rb
+++ app/controllers/work_experiences_controller.rb
@@ -1,8 +1,12 @@
 class WorkExperiencesController < ApplicationController
+  skip_before_action :login_required, only: [:index, :show]
+  
   def index
+    @work_experiences = WorkExperience.all
   end
 
   def show
+    @work_experience = WorkExperience.find(params[:id])
   end
 
   def new
@@ -13,7 +17,7 @@ class WorkExperiencesController < ApplicationController
     @work_experience = WorkExperience.new(we_params)
     
     if @work_experience.save
-      redirect_to work_experience_path, notice: "記事「#{@work_experience.title}」を投稿しました。"
+      redirect_to @work_experience, notice: "記事「#{@work_experience.title}」を投稿しました。"
     else
       render :new
     end
