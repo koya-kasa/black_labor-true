45dd84aba0cfc614f91144b72151877b0878e899
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2019-07-23 13:13:46 +0000

    curren_userの追加とwe_paramsの修正(途中)

--- app/controllers/work_experiences_controller.rb
+++ app/controllers/work_experiences_controller.rb
@@ -1,4 +1,5 @@
 class WorkExperiencesController < ApplicationController
+  #途中
   skip_before_action :login_required, only: [:index, :show]
   
   def index
@@ -14,7 +15,7 @@ class WorkExperiencesController < ApplicationController
   end
   
   def create
-    @work_experience = WorkExperience.new(we_params)
+    @work_experience = current_user.work_experiences.new(we_params)
     
     if @work_experience.save
       redirect_to @work_experience, notice: "記事「#{@work_experience.title}」を投稿しました。"
@@ -29,6 +30,6 @@ class WorkExperiencesController < ApplicationController
   private
   
   def we_params
-    params.require(:work_experience).permit(:title, :body, :user_id)
+    params.require(:work_experience).permit(:title, :body)
   end
 end
