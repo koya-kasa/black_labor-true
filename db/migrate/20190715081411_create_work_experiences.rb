4427b00703692afc5b3bff651bd3e56720e91a62
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2019-07-23 06:09:12 +0000

    weテーブルのマイグレーションファイル

--- db/migrate/20190715081411_create_work_experiences.rb
+++ db/migrate/20190715081411_create_work_experiences.rb
@@ -1,9 +1,9 @@
 class CreateWorkExperiences < ActiveRecord::Migration[5.2]
   def change
     create_table :work_experiences do |t|
-      t.string :title, unique: true, limit: 35, null: false
+      t.string :title, unique: true, limit: 35, null: false, index: true
       t.text :body, limit: 10000, unique: true, null: false
-      t.integer :user_id, null: false
+      t.integer :user_id, null: false, index: true
 
       t.timestamps
     end
