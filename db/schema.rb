9178eb2aed68236c7fa84661ca3d6b690418f0fd
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2019-07-26 02:36:38 +0000

    schmaファイルの変更

--- db/schema.rb
+++ db/schema.rb
@@ -29,11 +29,12 @@ ActiveRecord::Schema.define(version: 2019_07_15_081411) do
   create_table "work_experiences", force: :cascade do |t|
     t.string "title", limit: 35, null: false
     t.text "body", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["title"], name: "index_work_experiences_on_title"
     t.index ["user_id"], name: "index_work_experiences_on_user_id"
   end
 
+  add_foreign_key "work_experiences", "users"
 end
