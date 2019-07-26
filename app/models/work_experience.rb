657dfeb89a6d393b51c5e9d5cff7853afb0a81d5
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2019-07-26 02:33:29 +0000

    user_idのvalidationの消去

--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -1,7 +1,6 @@
 class WorkExperience < ApplicationRecord
     validates :title, presence: true, uniqueness: true, length: { in: 1..35 }
     validates :body, presence: true, uniqueness: true, length: { in: 1..10000 }
-    validates :user_id, presence: true
     
     belongs_to :user
 end
