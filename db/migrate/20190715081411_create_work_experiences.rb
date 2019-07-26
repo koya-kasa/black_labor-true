5d7d74e3666dcc59f8000ba9822df315045e3f2c
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2019-07-26 02:35:40 +0000

    外部キー制約の適応

--- db/migrate/20190715081411_create_work_experiences.rb
+++ db/migrate/20190715081411_create_work_experiences.rb
@@ -3,7 +3,7 @@ class CreateWorkExperiences < ActiveRecord::Migration[5.2]
     create_table :work_experiences do |t|
       t.string :title, unique: true, limit: 35, null: false, index: true
       t.text :body, limit: 10000, unique: true, null: false
-      t.integer :user_id, null: false, index: true
+      t.references :user, foreign_key: true, null: false
 
       t.timestamps
     end
