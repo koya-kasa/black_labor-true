73926aba8542d64db1438a02335df5abcdc0a2fb
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2019-08-02 03:53:50 +0000

    post_userメソッドの作成

--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -3,4 +3,8 @@ class WorkExperience < ApplicationRecord
     validates :body, presence: true, uniqueness: true, length: { in: 1..10000 }
     
     belongs_to :user
+    
+    def post_user
+        return User.find_by(id: self.user_id)
+    end
 end
