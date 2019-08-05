01e213c45062f671c529ebecfb2cf23c11db5111
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2019-08-05 04:56:26 +0000

    タグ機能の関連付け

--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -1,6 +1,9 @@
 class WorkExperience < ApplicationRecord
     validates :title, presence: true, uniqueness: true, length: { in: 1..35 }
     validates :body, presence: true, uniqueness: true, length: { in: 1..10000 }
+    validates :tag_list, presence: true, length: { in: 1..30 }
     
     belongs_to :user
+    
+    acts_as_taggable
 end
