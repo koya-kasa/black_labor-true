bf567e5870072e5fe02a2568673e10422747f10b
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2019-08-24 16:31:22 +0000

    allの消去、ストロングパラメータのmergeメソッド

--- app/controllers/work_experience_comments_controller.rb
+++ app/controllers/work_experience_comments_controller.rb
@@ -1,9 +1,8 @@
 class WorkExperienceCommentsController < ApplicationController
   def create
     @work_experience = WorkExperience.find(params[:work_experience_id])
-    @work_experience_comments = @work_experience.work_experience_comments.all
+    @work_experience_comments = @work_experience.work_experience_comments
     @work_experience_comment = current_user.work_experience_comments.new(wec_params)
-    @work_experience_comment.work_experience_id = @work_experience.id
     
     if @work_experience_comment.save
       redirect_to work_experience_path(@work_experience), notice: "感想を投稿しました。"
@@ -15,6 +14,6 @@ class WorkExperienceCommentsController < ApplicationController
   private
   
   def wec_params
-    params.require(:work_experience_comment).permit(:body)
+    params.require(:work_experience_comment).permit(:body).merge(work_experience_id: @work_experience.id)
   end
 end
