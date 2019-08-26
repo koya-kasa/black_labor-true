194ad825b04dc7ec33f8ee1b47b32d3900176fb7
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2019-08-26 06:38:09 +0000

    present?メソッドに変更

--- app/controllers/work_experience_comments_controller.rb
+++ app/controllers/work_experience_comments_controller.rb
@@ -1,7 +1,7 @@
 class WorkExperienceCommentsController < ApplicationController
   def create
     @work_experience = WorkExperience.find(params[:work_experience_id])
-    if current_user.work_experiences.count != 0
+    if current_user.work_experiences.present?
       @work_experience_comments = @work_experience.work_experience_comments.page(params[:page]).per(5)
     else
       @work_experience_comments = @work_experience.work_experience_comments.limit(5)
