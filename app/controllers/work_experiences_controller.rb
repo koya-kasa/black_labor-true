5cde572f474f0278436f2ad87d768691cccced90
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2019-08-29 15:29:53 +0000

    &.の追加

--- app/controllers/work_experiences_controller.rb
+++ app/controllers/work_experiences_controller.rb
@@ -14,7 +14,7 @@ class WorkExperiencesController < ApplicationController
 
   def show
     @work_experience = WorkExperience.find(params[:id])
-    if current_user.work_experiences.present?
+    if current_user&.work_experiences.present?
       @work_experience_comments = @work_experience.work_experience_comments.page(params[:page]).per(5)
     else
       @work_experience_comments = @work_experience.work_experience_comments.limit(5)
