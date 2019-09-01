eed1372c8a5114a64315623f703ed4cddf05379c
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2019-09-01 13:26:00 +0000

    インスタンス変数の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @work_experiences = @user.work_experiences.page(params[:page]).per(5)
+    @likes = @user.likes.page(params[:page]).per(5)
   end
   
   def new
