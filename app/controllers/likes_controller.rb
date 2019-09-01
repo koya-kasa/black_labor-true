6e1071c53323d6adb18794275a86bd1c581df83e
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2019-09-01 13:37:02 +0000

    createアクションの追加

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,13 @@
+class LikesController < ApplicationController
+    def create
+        return redirect_to work_experiences_path, notice: 'その記事は既にお気に入り登録されています。' if current_user.likes.find_by(work_experience_id: params[:work_experience_id]).present?
+        
+        @like = current_user.likes.new(work_experience_id: params[:work_experience_id])
+        
+        if @like.save
+            redirect_to user_path(@like.user), notice: "記事「#{@like.work_experience.title}」をお気に入りに追加しました。"
+        else
+            render 'users/show'
+        end
+    end
+end
