1ee2be837106f9c1ce66e683b922300813bb8d3d
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2019-09-06 11:06:46 +0000

    likesのあるなしの判定メソッドの追記

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
     belongs_to :user
     belongs_to :work_experience
+    
+    def check_current_user_likes(we_id)
+        current_user && current_user.likes.find_by(work_experience_id: we_id)
+    end
 end
