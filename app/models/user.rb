1f956f2aee416c148a06616e40fc2781d60c7a3d
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2019-09-18 08:26:18 +0000

    閲覧履歴のアソシエーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
     has_many :work_experiences
     has_many :work_experience_comments
     has_many :likes
-    
+    has_many :work_experience_looked_histories
     
     def check_current_user_likes_blank?(we_id)
         self.likes.find_by(work_experience_id: we_id).blank?
