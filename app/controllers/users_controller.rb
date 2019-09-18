3ad7316a6500578d0d44f286d1b45508aefc9fb6
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2019-09-18 12:51:49 +0000

    各一覧の並び順の指定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,8 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @work_experiences = @user.work_experiences.page(params[:page]).per(5)
-    @likes = @user.likes.page(params[:page]).per(5)
+    @work_experiences = @user.work_experiences.page(params[:page]).per(5).order(created_at: :desc)
+    @likes = @user.likes.page(params[:page]).per(5).order(created_at: :desc)
   end
   
   def new
