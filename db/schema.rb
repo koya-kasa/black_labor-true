8d6b7fee6264865ad4ad4d77e5db9883070363f6
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2019-09-27 09:32:11 +0000

    テーブル作成の反映

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_27_041428) do
+ActiveRecord::Schema.define(version: 2019_09_27_064945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2019_09_27_041428) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_likes_on_user_id"
     t.index ["work_experience_id"], name: "index_likes_on_work_experience_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "email", null: false
+    t.text "body", null: false
+    t.bigint "faq_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["faq_id"], name: "index_questions_on_faq_id"
   end
 
   create_table "taggings", id: :serial, force: :cascade do |t|
@@ -107,6 +116,7 @@ ActiveRecord::Schema.define(version: 2019_09_27_041428) do
 
   add_foreign_key "likes", "users"
   add_foreign_key "likes", "work_experiences"
+  add_foreign_key "questions", "faqs"
   add_foreign_key "work_experience_coments", "users"
   add_foreign_key "work_experience_coments", "work_experiences"
   add_foreign_key "work_experience_comments", "users"
